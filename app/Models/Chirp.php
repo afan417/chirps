5399cb763f6ea734b43bb2c09f2a21b6ea7757f0
Afan Ajdari <batman.two@hotmail.com>
2023-08-14 19:12:19 +0200

    first commit

---
+++ app/Models/Chirp.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Events\ChirpCreated;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Model;
+
+class Chirp extends Model
+{
+    protected $fillable = [
+        'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+
+    use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
